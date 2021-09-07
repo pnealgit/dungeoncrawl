7e0bac07b3339e074035bcb4f3fada70302da524
Phillip Neal <philn1984@gmail.com>
2021-09-07 08:36:19 +0200

    stuck in turns but bad player turn

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod camera;
 mod components;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -23,6 +24,7 @@ mod prelude {
     pub use crate::components::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
@@ -30,7 +32,9 @@ use prelude::*;
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule
 }
 
 impl State {
@@ -47,12 +51,14 @@ impl State {
             .for_each(|pos| spawn_monster(&mut ecs, &mut rng,pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
-
+        resources.insert(TurnState::AwaitingInput);
 
         Self {
             ecs,
             resources,
-            systems: build_scheduler()
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler()
         }
     }
 }
@@ -64,7 +70,21 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        //self.systems.execute(&mut self.ecs, &mut self.resources);
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput => self.input_systems.execute(
+                &mut self.ecs,
+                &mut self.resources
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+            TurnState::MonsterTurn => {
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+
+        }
         render_draw_buffer(ctx).expect("Render error");
 
     }
@@ -78,7 +98,7 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32) // (3)
         .with_resource_path("resources/") // (4)
         .with_font("dungeonfont.png", 32, 32) // (5)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
             "dungeonfont.png") // (7)
         .build()?;
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,3 +1,4 @@
+
 use crate::prelude::*;
 #[system]
 #[write_component(Point)]
@@ -7,7 +8,8 @@ pub fn player_input(
     ecs: &mut SubWorld,
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState
 ) 
 {
     if let Some(key) = key {
@@ -27,6 +29,7 @@ pub fn player_input(
                 if map.can_enter_tile(destination) {
                     *pos = destination;
                     camera.on_player_move(destination);
+                    *turn_state = TurnState::PlayerTurn;
                 } //end of if on map
             }); //end of iter
         } //end of if delta x, deltay
